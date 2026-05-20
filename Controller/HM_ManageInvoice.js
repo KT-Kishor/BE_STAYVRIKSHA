8a5a5e0cba32854fec67d51b79ef59059bc64ff4
amit3712 <kamit7102@gmail.com>
2026-05-20 15:12:00 +0530

    manage invoice changes

--- Controller/HM_ManageInvoice.js
+++ Controller/HM_ManageInvoice.js
@@ -974,7 +974,7 @@ function calculateBookingCycleAmounts(bookings,cycleStart,cycleEnd,invoiceIndex)
       }
 
       // ONCE_PER_BOOKING
-      else if (chargeType === "ONCE_PER_BOOKING") {
+      else if (chargeType === "Once") {
         if (invoiceIndex > 0) return;
 
         facilityAmount = truncate2(price);
