5b079a2ac12259db16013e7f8b75e3e337a1a90d
Pavan Kadam <pavan.kadam@kalpavrikshatechnologies.com>
2026-07-30 18:59:02 +0530

    Add Payment field to postHM_InvoicePaymentDetail for enhanced payment tracking

--- Controller/HM_ManageInvoice.js
+++ Controller/HM_ManageInvoice.js
@@ -603,6 +603,7 @@ async function postHM_InvoicePaymentDetail(req, res, next) {
       BranchCode: inputData.BranchCode,
       InvNo: inputData.InvNo,
       EntryDate: inputData.EntryDate,
+      Payment: inputData.Payment
     };
 
     //  CONDITION: mark Used only if no prior payment exists
